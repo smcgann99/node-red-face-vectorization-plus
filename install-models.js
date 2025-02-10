1fb3d44c258f36b7a8cf0fc9b7791a1ddc3aca32
smcgann99 <52132596+smcgann99@users.noreply.github.com>
2025-02-10 17:29:03 +0000

    Add files via upload

---
+++ install-models.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const modelUrls = {
+  "facenet-model":
+    "https://github.com/GOOD-I-DEER/node-red-contrib-face-vectorization/raw/main/model/facenet-model.onnx",
+};
+
+const downloadDir = path.join(__dirname, "model");
+
+async function downloadModels() {
+  try {
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir, { recursive: true });
+    }
+
+    for (const modelName in modelUrls) {
+      const modelUrl = modelUrls[modelName];
+      const response = await axios.get(modelUrl, { responseType: "stream" });
+      const modelPath = path.join(downloadDir, `${modelName}.onnx`);
+      const fileStream = fs.createWriteStream(modelPath);
+      response.data.pipe(fileStream);
+      await new Promise((resolve, reject) => {
+        fileStream.on("finish", resolve);
+        fileStream.on("error", reject);
+      });
+      console.log(`Model file ${modelName}.onnx Download Complete`);
+    }
+  } catch (error) {
+    console.error("Error downloading model file:", error);
+  }
+}
+
+downloadModels();
